1cd553628e7f4d5228872d164e61ad2f5f65d643
adit-bala <adityarbala@gmail.com>
2022-08-15 22:18:12 -0400

    Updates

---
+++ precache-manifest.2316c290297b15e2c0f57a3ef2828ccd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "94f14cec0a122cea4fb7",
+    "url": "./static/css/main.308aa796.chunk.css"
+  },
+  {
+    "revision": "94f14cec0a122cea4fb7",
+    "url": "./static/js/main.94f14cec.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "./static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "83c2bc175fec0a7d8dc7",
+    "url": "./static/js/2.83c2bc17.chunk.js"
+  },
+  {
+    "revision": "acd1742253593da3fb82",
+    "url": "./static/js/runtime~main.acd17422.js"
+  },
+  {
+    "revision": "960a3d75a2e797252daa433941fc63eb",
+    "url": "./static/media/headshot.960a3d75.png"
+  },
+  {
+    "revision": "b8132cc8aa85b3564529644c8d1fa225",
+    "url": "./static/media/tongueout.b8132cc8.png"
+  },
+  {
+    "revision": "54e34b62f855498f6d15e501d7eabbd7",
+    "url": "./index.html"
+  }
+];
