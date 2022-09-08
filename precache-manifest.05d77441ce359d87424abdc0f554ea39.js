64e02ef57045af1ed76168301dc833b119a1d1c6
adit-bala <adityarbala@gmail.com>
2022-09-07 22:06:08 -0700

    Updates

--- precache-manifest.05d77441ce359d87424abdc0f554ea39.js
+++ precache-manifest.05d77441ce359d87424abdc0f554ea39.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "4b0074517a0afa52ce6a",
+    "revision": "57fa8ff450b1d961ff81",
     "url": "./static/css/main.308aa796.chunk.css"
   },
   {
-    "revision": "4b0074517a0afa52ce6a",
-    "url": "./static/js/main.4b007451.chunk.js"
+    "revision": "57fa8ff450b1d961ff81",
+    "url": "./static/js/main.57fa8ff4.chunk.js"
   },
   {
     "revision": "9863931913954e89bf2e",
@@ -20,15 +20,15 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.acd17422.js"
   },
   {
-    "revision": "b8132cc8aa85b3564529644c8d1fa225",
-    "url": "./static/media/tongueout.b8132cc8.png"
-  },
-  {
     "revision": "960a3d75a2e797252daa433941fc63eb",
     "url": "./static/media/headshot.960a3d75.png"
   },
   {
-    "revision": "d770d6550155831c0dc926825e05800a",
+    "revision": "b8132cc8aa85b3564529644c8d1fa225",
+    "url": "./static/media/tongueout.b8132cc8.png"
+  },
+  {
+    "revision": "530d644938416b46b91a8a5469c85812",
     "url": "./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7d97161ef6fded12902b1bf6c0533cc4.js"
+  "./precache-manifest.05d77441ce359d87424abdc0f554ea39.js"
 );
 
 workbox.clientsClaim();
